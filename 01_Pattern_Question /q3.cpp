dea161e0ff935d837a034ffc12c969f52dbd3d60
Kumar Shivam <kumarshivamtiwari0@gmail.com>
2026-08-30 11:14:56 +0000

    q3

---
+++ 01_Pattern_Question /q3.cpp
@@ -0,0 +1,25 @@
+/*
+1
+1 2
+1 2 3 
+1 2 3 4
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+void print1(int n){
+     for (int i = 1; i <=n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << j << " ";
+        }
+        cout << endl;
+    }
+}
+
+int main(){
+    int n;
+    cin>>n;
+    print1(n);
+}
