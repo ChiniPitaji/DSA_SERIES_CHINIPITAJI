8b74583f732d8bfb5bb56e9ae1e005828d9087d8
Kumar Shivam <kumarshivamtiwari0@gmail.com>
2026-08-30 11:02:08 +0000

    Pattern question 1

--- 01_Pattern_Question /q1.cpp
+++ 01_Pattern_Question /q1.cpp
@@ -1,22 +1,45 @@
-/* 
+/*
 
 ****
 ****
 ****
-**** 
+****
 
 */
+// WAY --> 1 
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     for (int i = 0; i < 4; i++)
+//     {
+//         for (int j = 0; j < 4; j++)
+//         {
+//             cout << "*";
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
 
 
-
-#include <iostream>
+//WAY -->2
+#include <bits/stdc++.h>
 using namespace std;
+void print1(int n){
+     for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+}
 
 int main(){
-    for(int i=0;i<4;i++){
-        for(int j=0;j<4;j++){
-            cout<<"*";
-        }
-        cout<<endl;
-    }
+    int n;
+    cin>>n;
+    print1(n);
 }
