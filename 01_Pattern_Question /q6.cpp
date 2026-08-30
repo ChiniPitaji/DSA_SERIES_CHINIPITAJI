7ad5838e1ac0b0279a969a241639376173a2e0c3
Kumar Shivam <kumarshivamtiwari0@gmail.com>
2026-08-30 11:30:35 +0000

    q6

--- 01_Pattern_Question /q6.cpp
+++ 01_Pattern_Question /q6.cpp
@@ -1,13 +1,21 @@
+/*
+1 2 3 
+1 2 
+1 
+*/
+
+
 #include <bits/stdc++.h>
 using namespace std;
 void print1(int n){
      for (int i = 1; i <=n; i++)
     {
-       for(int j=0;j<n-i+1;j++){
-        cout<<"* ";
+       for(int j=1;j<=n-i+1;j++){
+        cout<<j<<" ";
        }
        cout<<endl;
     }
+}
 
 int main() {
     int n;
